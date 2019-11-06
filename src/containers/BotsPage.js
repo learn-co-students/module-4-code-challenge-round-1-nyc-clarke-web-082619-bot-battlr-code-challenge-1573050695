046b5431e7d0fd2cefa2f2891a80cf43fba8f8a3
github username <github email address>
2019-11-06 11:03:39 -0500

    attempted filter

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import BotCollection from './BotCollection';
 import YourBotArmy from './YourBotArmy';
 import BotSpecs from '../components/BotSpecs';
-import { Search } from 'semantic-ui-react'
+import { Search, Dropdown } from 'semantic-ui-react'
 const API = "https://bot-battler-api.herokuapp.com/api/v1/bots";
 
 class BotsPage extends React.Component {
@@ -12,7 +12,8 @@ class BotsPage extends React.Component {
     botsArmy: new Set(),
     showBotBool: false,
     botToShow: {},
-    searchTerm: ""
+    searchTerm: "",
+    filter: ""
   }
 
   componentDidMount() {
@@ -60,6 +61,16 @@ class BotsPage extends React.Component {
           <BotSpecs goBack={this.goBack} addBotToArmy={this.addBotToArmy} {...this.state.botToShow}/> 
         : 
           <div>
+            {/* <Dropdown 
+            class="filter"
+            value={this.state.filter}
+            options={"Assault"}
+
+            onChange={e => {
+              this.setState({ city: e.value });
+            }}
+            placeholder='Select a Type'
+				    /> */}
             <Search className="searchBar" value={this.state.searchTerm} onSearchChange={((event,value) => this.searchBots(event,value))} showNoResults={false}/>
             <BotCollection searchTerm={this.state.searchTerm} showBotSpecs={this.showBotSpecs} bots={this.state.bots} /> 
           </div>
