0fc7cb2e2109cf4eb7a2984ba691e45ff1370468
github username <github email address>
2019-11-06 10:27:50 -0500

    andrew

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,16 +1,53 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from '../components/BotSpecs'
+
 
 class BotCollection extends React.Component {
   //your code here
+	state = {
+		currentBot: {},
+	}
+
+	showBot = (bot) => {
+		this.setState({
+			currentBot: bot
+		})
+	}
+
+	removeBot = () => {
+		this.setState({
+			currentBot: {}
+		})
+	}
+
+	renderBots = () => {
+		if(!this.state.currentBot.id){
+			return (
+			<div className="row">
+    		  Collection of all bots
+				{this.props.bots.map(bot => 
+				<BotCard 
+				key={bot.id} 
+				bot={bot} 
+				showBot={this.showBot}
+				/>)}
+    		</div>
+			)
+		} else {
+			return(
+				<BotSpecs
+				bot={this.state.currentBot}
+				goBack={this.removeBot}
+				joinArmy={this.props.joinArmy}
+				/>)
+		}
+	}
 
   render(){
   	return (
   	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
+    		{this.renderBots()}
   	  </div>
   	);
   }
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,45 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+
+const API = `https://bot-battler-api.herokuapp.com/api/v1/bots`
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    bots: [],
+    myArmy: [],
+  }
+
+
+  componentDidMount(){
+    fetch(API)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          bots: data
+        })
+      })
+  }
+
+  joinArmy = (bot) => {
+    if(this.state.myArmy.includes(bot)){
+      null
+    } else {
+      this.setState({
+        myArmy: [...this.state.myArmy, bot]
+      })
+    }
+  }
+
+
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <BotCollection bots={this.state.bots} myArmy={this.state.myArmy} joinArmy={this.joinArmy}/>
+        <YourBotArmy bots={this.state.myArmy}/>
       </div>
     );
   }
